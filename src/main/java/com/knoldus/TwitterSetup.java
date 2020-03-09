2266cfe596c8cc23ab12df0cc91b92e1869f382d
mansi7babbar <mansi7babbar@gmail.com>
2020-03-09 11:02:10 +0530

    updated twitter setup

--- src/main/java/com/knoldus/TwitterSetup.java
+++ src/main/java/com/knoldus/TwitterSetup.java
@@ -11,10 +11,10 @@ public class TwitterSetup {
     Twitter getTwitterInstance() {
         ConfigurationBuilder config = new ConfigurationBuilder();
         config.setDebugEnabled(true)
-                .setOAuthConsumerKey("e6uS4phTxImI68qlA6h4V3zwR")
-                .setOAuthConsumerSecret("M8b4Q3sudgU9mNZgJx1onUlqQYi5h5YCK1GVacjAc8yHDAohFc")
-                .setOAuthAccessToken("160922224-AKOoOasbqi3huqT7uyq4Og0Oqlucn8rKeD9IcUvU")
-                .setOAuthAccessTokenSecret("7HgIJUmjOX2AZThvVp7RPWsZwOrW1ffpvkEpjeBSQynnH");
+                .setOAuthConsumerKey(consumer.key)
+                .setOAuthConsumerSecret(consumer.secret)
+                .setOAuthAccessToken(token.key)
+                .setOAuthAccessTokenSecret(token.secret);
         TwitterFactory twitterFactory = new TwitterFactory(config.build());
         return twitterFactory.getInstance();
     }
