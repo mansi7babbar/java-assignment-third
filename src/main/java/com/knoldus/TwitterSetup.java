d29680c13d64341540869db19829e203dc8bd6a8
mansi7babbar <mansi7babbar@gmail.com>
2020-03-09 12:19:06 +0530

    updated application.conf

--- src/main/java/com/knoldus/TwitterSetup.java
+++ src/main/java/com/knoldus/TwitterSetup.java
@@ -1,30 +1,23 @@
 package com.knoldus;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 /**
  * TwitterSetup is created to generate a twitter instance.
  */
 public class TwitterSetup {
-    Twitter getTwitterInstance() throws IOException {
-        InputStream input = new FileInputStream("path/to/config.properties");
-        Properties prop = new Properties();
-        prop.load(input);
-
+    Twitter getTwitterInstance() {
+        Config configFactory = ConfigFactory.load();
         ConfigurationBuilder config = new ConfigurationBuilder();
         config.setDebugEnabled(true)
-                .setOAuthConsumerKey(prop.getProperty("consumer.key"))
-                .setOAuthConsumerSecret("consumer.secret")
-                .setOAuthAccessToken("token.key")
-                .setOAuthAccessTokenSecret("token.secret");
-
+                .setOAuthConsumerKey(configFactory.getString("consumer.key"))
+                .setOAuthConsumerSecret(configFactory.getString("consumer.secret"))
+                .setOAuthAccessToken(configFactory.getString("token.key"))
+                .setOAuthAccessTokenSecret(configFactory.getString("token.secret"));
         TwitterFactory twitterFactory = new TwitterFactory(config.build());
         return twitterFactory.getInstance();
     }
